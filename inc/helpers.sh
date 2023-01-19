e3a9db21f7b4e3b013eb2aded79effa1dd7cecb3
Eric Mulder <eric@labelvier.nl>
2023-01-19 13:06:26 +0100

    refactor functions to include documentation, and every subfile also uses a main function now and a wrapper

--- inc/helpers.sh
+++ inc/helpers.sh
@@ -8,15 +8,21 @@ function _needs_active_wptakeoff_project() {
   fi
 }
 
-function _echo_local_functions() {
+function _echo_documentation() {
   # first arguments is the name of the file
   local contents=$(cat $1)
-  # get all lines starting with "function" and ending with "{" and save them in an array
-  local functions=($(echo "$contents" | grep -oE 'function [^(]*'))
-  # use sed to remove the function keyword and the space
-  local functions=($(echo "${functions[@]}" | sed -e 's/function //g'))
+  # get all lines starting with any whitespace, then # @function and the next line
+  local functions=$(echo "$contents" | grep -E "^\s*# @function" -A 1)
   echo "Available functions:"
-  for i in "${!functions[@]}"; do
-    echo "$i) ${functions[$i]}"
-  done
+  # loop through all functions
+  while read -r line; do
+    # get the name of the function
+    local function_name=$(echo "$line" | cut -d' ' -f3-)
+    # get the description of the function (the next line)
+    read -r line
+    local function_description=$(echo "$line" | cut -d' ' -f3-)
+    # print the name and description
+    echo "  $function_name - $function_description"
+    read -r line
+  done <<< "$functions"
 }
---
+++ inc/starterkit.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+starterkit() (
+
+  # Local filename to echo the documentation.
+  local filename="starterkit.sh"
+
+  # Runs the command.
+  function main() {
+    # try to run the subcommand passed as the second argument and that function exists
+    if [[ -n "$1" ]] && type -t "$1" | grep -q 'function'; then
+      "$1"
+    else
+      # if no subcommand is passed, run the documentation function
+      _echo_documentation "inc/$filename"
+    fi
+  }
+
+  # @function install
+  # @description Installs the starterkit.
+  function install() {
+    # Ask for the project name
+    read -p "What is the name of your project (root folder)? " project_name
+    # exit if no project name is given
+    if [[ -z "$project_name" ]]; then
+      echo "No project name given. Exiting."
+      exit 1
+    fi
+
+    # Ask which branch to use (default master)
+    read -p "Which branch do you want to use? (default: master) " branch
+    branch=${branch:-master}
+
+    # Ask what the theme name should be
+    read -p "What should the theme name be? (default: labelvier) " theme_name
+    theme_name=${theme_name:-labelvier}
+
+    # Download and install the latest version of the wp-takeoff starter kit with a depth of 1
+    git clone -b $branch --single-branch --depth 1 git@bitbucket.org:labelvier/wordpress-starter-kit.git $project_name || exit 1
+    cd $project_name || exit 1
+    rm -rf .git
+
+    # Rename the theme folder wp-content/themes/labelvier to the theme name
+    mv wp-content/themes/labelvier wp-content/themes/$theme_name
+
+    # in the example.env file replace the theme name for the lines which start with THEME_FOLDER_NAME DEV_THEME_PATH
+    sed -i '' "s/labelvier/$theme_name/g" example.env
+
+    # Copy the example.env file to .env
+    cp example.env .env
+
+    # Install the dependencies
+    echo "Installing dependencies... (npm install)"
+    npm run install || exit 1
+
+    # Create a new git repository (don't output the output)
+    git init
+    git add .
+    git commit -m "Initial commit" > /dev/null
+
+    echo "Installation complete"
+  }
+
+  main "$@"
+)
