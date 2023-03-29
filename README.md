8f6963ad62b803740221bc620d61cf502bff78a9
Eric Mulder <eric@labelvier.nl>
2023-03-29 11:46:23 +0200

    new deploy command which merges all feature branches in a staging branch and runs the npm run deploy-staging command after that

--- README.md
+++ README.md
@@ -44,7 +44,8 @@ command_name() (
   function main() {
     # try to run the subcommand passed as the second argument and that function exists
     if [[ -n "$1" ]] && type -t "$1" | grep -q 'function'; then
-      "$1"
+      # attach any remaining arguments to the function
+      "$1" "${@:2}"
     else
       # if no subcommand is passed, run the documentation function
       _echo_documentation "$filename"
---
+++ inc/deploy.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+deploy() (
+
+  # Local filename to echo the documentation.
+  local filename="deploy.sh"
+  # get the current directory name of this file
+  local current_dir=$(dirname "${BASH_SOURCE[0]}")
+
+  # Runs the command.
+  function main() {
+    # try to run the subcommand passed as the second argument and that function exists
+    if [[ -n "$1" ]] && type -t "$1" | grep -q 'function'; then
+      # attach any remaining arguments to the function
+      "$1" "${@:2}"
+    else
+      # if no subcommand is passed, run the documentation function
+      _echo_documentation "$filename"
+    fi
+  }
+
+  # @function staging
+  # @description deploys the staging branch to the staging environment. Use --all-features to deploy all feature/ branches to staging.
+  function staging() {
+    # check if there are any uncommitted changes, if so exit with message
+    if [[ $(git status | grep "Changes not staged for commit") ]]; then
+      echo "There are uncommitted changes, please commit them and try again"
+      exit 1
+    fi
+
+    # check if there is a staging branch, if not create it
+    if [[ -z $(git branch --list staging) ]]; then
+      git checkout -b staging
+    else
+      git checkout staging
+    fi
+
+
+    # check if we are on the staging branch, if not exit with message
+    if [[ $(git branch --list | grep \* | sed 's/\* //g') != "staging" ]]; then
+      echo "You are not on the staging branch, please check any errors and try again"
+      exit 1
+    fi
+
+    # do a pull to make sure we have the latest changes
+    git pull origin staging
+
+    # check if the --all-features flag is passed
+    if [[ "$1" == "--all-features" ]]; then
+      # if so, deploy all feature branches to staging
+      echo "Deploying all feature branches to staging"
+      # get all feature branches
+      local feature_branches=$(git branch --list feature/* | sed 's/feature\///g')
+      # loop through all feature branches
+      for feature_branch in $feature_branches; do
+        # merge the feature branch into staging
+        echo "Merge feature/$feature_branch into staging"
+        git merge --no-ff --no-edit feature/$feature_branch
+      done
+    else
+      # if not, deploy the current branch to staging
+      git merge --no-ff --no-edit
+    fi
+
+    # check if we have a merge conflict, if so exit with message
+    if [[ $(git status | grep "both modified") ]]; then
+      echo "There is a merge conflict, please resolve it and try again"
+      exit 1
+    fi
+
+    # push the staging branch to the remote
+    git push origin staging
+
+    # check if the npm run deploy-staging exists
+    npm run deploy-staging
+
+    #switch back to the previous branch
+    git checkout -
+  }
+
+  main "$@"
+)
