d6a5c055ad77a042ae339cfb41db8fd5deb0d95e
Eric Mulder <eric@labelvier.nl>
2023-09-12 16:27:33 +0200

    fix debug check with display errors

--- inc/migrate.sh
+++ inc/migrate.sh
@@ -286,7 +286,7 @@ migrate() (
   if [[ ! -z "$display_errors" ]]; then
     # check if define( 'WP_DEBUG', false ); is in the wp-config.php file on the destination server
     wp_debug=$(ssh -p "$ssh_port_destination" "$ssh_username_destination@$ssh_hostname_destination" "grep -o \"define( 'WP_DEBUG', false )\" $ssh_path_destination/wp-config.php")
-    if [[ -n $wp_debug ]]; then
+    if [[ ! -z $wp_debug ]]; then
       # replace define( 'WP_DEBUG', false ); with define( 'WP_DEBUG', false );ini_set('display_errors', '0');
       echo "Replacing define( 'WP_DEBUG', false ); with define( 'WP_DEBUG', false );ini_set('display_errors', 0); in the wp-config.php file on the destination server..."
       ssh -p "$ssh_port_destination" "$ssh_username_destination@$ssh_hostname_destination" "sed -i \"s/define( 'WP_DEBUG', false );/define( 'WP_DEBUG', false );ini_set('display_errors', '0');/g\" $ssh_path_destination/wp-config.php"
